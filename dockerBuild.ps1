9297b9d3917dc6e6e01396aa2119c3714db735d5
Benuhx <15267677+Benuhx@users.noreply.github.com>
2020-07-05 03:44:29 +0200

    Docker aktualisiert. Image startet aber noch nicht

--- dockerBuild.ps1
+++ dockerBuild.ps1
@@ -1,3 +1,7 @@
-dotnet publish -c Release
-dotnet MedReminder/bin/Release/netcoreapp3.1/publish/MedReminder.dll
+Remove-Item "./Docker/files" -Recurse -ErrorAction Ignore
+dotnet publish --nologo -c Release -r=linux-arm -o "./Docker/files" --self-contained false
+cp config.yaml.template Docker/files/config.yaml
+
+cd Docker
 docker build -t counter-image -f Dockerfile .
+cd ..
