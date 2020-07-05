c739b5ddc8df3436702428e5dfb8914238fe80e1
Benuhx <15267677+Benuhx@users.noreply.github.com>
2020-07-05 20:03:45 +0200

    Dockerimage funktioniert auf arm (z.B. Raspberry pi)

--- dockerBuild.ps1
+++ dockerBuild.ps1
@@ -1,7 +1,16 @@
-Remove-Item "./Docker/files" -Recurse -ErrorAction Ignore
-dotnet publish --nologo -c Release -r=linux-arm -o "./Docker/files" --self-contained false
-cp config.yaml.template Docker/files/config.yaml
+Remove-Item "./Docker/filesLinux64" -Recurse -ErrorAction Ignore
+Remove-Item "./Docker/filesLinuxArm" -Recurse -ErrorAction Ignore
+
+dotnet publish -c Release -o "./Docker/filesLinux64" -r=linux-x64 --nologo --no-self-contained
+dotnet publish -c Release -o "./Docker/filesLinuxArm" -r=linux-arm --nologo --no-self-contained
 
 cd Docker
-docker build -t counter-image -f Dockerfile .
+docker build -f Dockerfile-x64 -t benuhx/med-reminder:latest .
+docker build -f Dockerfile-arm -t benuhx/med-reminder:latest-arm .
+# docker buildx build -f Dockerfile-arm --platform linux/arm/v7 -t benuhx/med-reminder:latest-arm .
 cd ..
+
+docker push benuhx/med-reminder:latest
+docker push benuhx/med-reminder:latest-arm
+
+docker image prune -f
